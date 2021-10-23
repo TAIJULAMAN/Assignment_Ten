0a174886c8011c061978501bfaf8cbc8fc460c59
TAIJULAMAN <taijulaman8@gmail.com>
2021-10-23 13:51:34 +0600

    last

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ass-10
+# My Website Name is : Yoga
+
+## Live Link : https://health-care-6a0b8.web.app
+
+### Yoga is the journey of the self,through the self, to the self
--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,3 +1,10 @@
 // akhane nijer firebase config boshbe
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAClf_uYZCdM8fENBDJoqmOMWXrqxOXkXo",
+  authDomain: "health-care-6a0b8.firebaseapp.com",
+  projectId: "health-care-6a0b8",
+  storageBucket: "health-care-6a0b8.appspot.com",
+  messagingSenderId: "788714059789",
+  appId: "1:788714059789:web:b938612adeb8a288337d44",
+};
 export default firebaseConfig;
